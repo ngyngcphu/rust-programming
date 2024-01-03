902d0df51043f57997f021305cdfd53a6ad09271
ngyngcphu <nguyenngocphu225@gmail.com>
2024-01-03 14:49:42 +0700

    feat(slice): practice slice type reference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,26 @@
-fn main() {
-    let reference_to_nothing = dangle();
-    println!("{reference_to_nothing}");
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
 
-fn dangle() -> String {
-    let s = String::from("hello");
-    s
+fn main() {
+   let my_string = String::from("hello world");
+
+   let word = first_word(&my_string[0..6]);
+   let word = first_word(&my_string[..]);
+   let word = first_word(&my_string);
+
+   let my_string_literal = "hello world";
+
+   let word = first_word(&my_string_literal[0..6]);
+   let word = first_word(&my_string_literal[..]);
+   let word = first_word(my_string_literal);
+
 }
