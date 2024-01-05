49ea07ddd159d1925420e26b61d03940aa8b2416
ngyngcphu <nguyenngocphu225@gmail.com>
2024-01-05 13:11:32 +0700

    feat(generic): practice defining generic type for struct

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -1,19 +1,21 @@
-fn largest(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-    for item in list {
-        if item > largest {
-            largest = item;
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
